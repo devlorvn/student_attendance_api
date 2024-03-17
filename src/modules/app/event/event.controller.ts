c770f43813354e2b14c3cb29943279f70076d47b
Saritoz <nddung1119@gmail.com>
2024-03-18 00:56:27 +0700

    add api attendance

--- src/modules/app/event/event.controller.ts
+++ src/modules/app/event/event.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Param, UseGuards, Query, Req } from "@nestjs/common";
+import { Controller, Get, Param, UseGuards, Query, Req, Patch, UseInterceptors, UploadedFile, Body } from "@nestjs/common";
 import { EventService } from "./event.service";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiBody, ApiConsumes, ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "src/common/guards";
 import { ListEventDto } from "./dto/list-event.dto";
 import { RequestWithUser } from "../auth/auth.interface";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { ExceptionFactory } from "src/common/exceptions/exceptionsFactory";
 
 @Controller("app/event")
 @ApiTags("Event")
@@ -29,5 +31,36 @@ export class EventController {
   @Get(":id")
   findOne(@Param("id") id: string, @Req() req: RequestWithUser) {
     return this.eventService.findOne(req.user.mssv, id);
+  }
+
+  @Patch("/attendance")
+  @ApiConsumes("multipart/form-data")
+  @ApiBody({
+    schema: {
+      required: ["eventId", "file"],
+      type: "object",
+      properties: {
+        eventId: {
+          type: "string",
+        },
+        file: {
+          type: "string",
+          format: "binary",
+        },
+      },
+    },
+  })
+  @UseInterceptors(FileInterceptor("file"))
+  async attendanceEvent(@UploadedFile() file: Express.Multer.File, @Body("eventId") eventId: string, @Req() { user }: RequestWithUser) {
+    const timestamp = Date.now();
+    const newFileName = `${file.originalname}_${timestamp}`;
+    const result = await this.eventService.uploadFilePrivate(file.buffer, newFileName);
+    if (result) {
+      return this.eventService.updateAttendance(user.mssv, eventId, result.url);
+    }
+    return ExceptionFactory.badRequestException({
+      message: "Lỗi",
+      errorCode: -1,
+    });
   }
 }
--- src/modules/app/event/event.service.ts
+++ src/modules/app/event/event.service.ts
@@ -5,6 +5,9 @@ import Event from "src/modules/manage/event/entities/event.entity";
 import Topic from "src/modules/manage/topic/entities/topic.entity";
 import { Student } from "src/modules/student/entities/student.entity";
 import { ListEventDto } from "./dto/list-event.dto";
+import { StorageService } from "src/common/providers/storage.service";
+import { ConfigService } from "@nestjs/config";
+import RegisterEvent from "src/modules/registerEvent/entities/registerEvent.entity";
 
 @Injectable()
 export class EventService {
@@ -12,7 +15,11 @@ export class EventService {
     @InjectRepository(Event)
     private eventRepository: Repository<Event>,
     @InjectRepository(Topic)
-    private topicRepository: Repository<Topic>
+    private topicRepository: Repository<Topic>,
+    @InjectRepository(RegisterEvent)
+    private registerEventRepository: Repository<RegisterEvent>,
+    private storageService: StorageService,
+    private readonly configService: ConfigService
   ) {}
 
   async findAll(mssv: Student["mssv"], { page, topic, registered }: ListEventDto) {
@@ -24,7 +31,7 @@ export class EventService {
         registers: {
           mssv: registered === "true" ? mssv : undefined,
         },
-        enable: true
+        enable: true,
       },
       take: 10,
       skip: (page - 1) * 10,
@@ -80,5 +87,25 @@ export class EventService {
       })),
       count,
     };
+  }
+
+  async uploadFilePrivate(imageBuffer: Buffer, filename: string) {
+    filename = `private/${filename}`;
+    await this.storageService.uploadPublicFile(imageBuffer, filename);
+    return {
+      url: `${this.configService.get("S3_HOST_URL")}/${filename}`,
+    };
+  }
+
+  async updateAttendance(mssv: Student["mssv"], eventId: Event["id"], imageURL: RegisterEvent["attendanceImage"]) {
+    const foundRegister = await this.registerEventRepository.findOne({
+      where: {
+        eventId: eventId,
+        mssv,
+      },
+    });
+    let updateResult: RegisterEvent;
+    if (foundRegister) updateResult = await this.registerEventRepository.save({ ...foundRegister, attendanceImage: imageURL });
+    return updateResult;
   }
 }
